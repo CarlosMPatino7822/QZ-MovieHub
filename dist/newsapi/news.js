0c643a5eda6bcfd2bbe139e22827602c80136be9
Juan Diego Velasco <velasco.juan.7583@eam.edu.co>
2025-10-24 07:18:19 -0500

    feat: add apis

--- dist/newsapi/news.js
+++ dist/newsapi/news.js
@@ -1,7 +1,7 @@
 const API_KEY = "6d30ac3094ba483db18fe40cb5ba81ef";
 export async function getMovieNews() {
     try {
-        const response = await fetch(`https://newsapi.org/v2/everything?q=cinema%20OR%20películas%20OR%20estrenos%20OR%20actores&language=es&sortBy=publishedAt&apiKey=${API_KEY}`);
+        const response = await fetch(`https://newsapi.org/v2/everything?q=cinema&language=es&apiKey=6d30ac3094ba483db18fe40cb5ba81ef`);
         if (!response.ok) {
             throw new Error("Error al obtener noticias del servidor");
         }
@@ -12,5 +12,10 @@ export async function getMovieNews() {
         console.error("Error en getMovieNews:", error);
         return [];
     }
+}
+export async function getNews() {
+    const res = await fetch(`https://newsapi.org/v2/everything?q=movies&language=es&apiKey=${API_KEY}`);
+    const data = await res.json();
+    return data.articles;
 }
 //# sourceMappingURL=news.js.map
